59797354b3773f62dc167a05d22e1306b88c533e
Sangyeop-Lim <dladla5675@gmail.com>
2025-06-24 19:13:46 +0900

    feat : 플레이어 컨트롤러 스크립트 추가 및 플레이어 이동 기능 구현
    - 플레이어 컨트롤러 스크립트 추가
    - 플레이어 이동 기능 구현
    - #1

---
+++ Assets/Folder_LSY/Scritps/PlayerController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+    private Vector3 targetPos;
+    private bool isMoving = false;
+
+    private void Update()
+    {
+        HandleInput();
+        HandleMovement();
+    }
+
+    private void HandleInput()
+    {
+        if (Input.GetMouseButtonDown(1))
+        {
+            Vector3 mousePos = Input.mousePosition;
+            mousePos.z = 10f;
+            targetPos = Camera.main.ScreenToWorldPoint(mousePos);
+            targetPos.z = 0f;
+            isMoving = true;
+        }
+    }
+
+    private void HandleMovement()
+    {
+        if (!isMoving) return;
+
+        Vector3 direction = (targetPos - transform.position).normalized;
+        float distance = Vector3.Distance(transform.position, targetPos);
+
+        if (distance > 0.1f)
+        {
+            transform.position += direction * moveSpeed * Time.deltaTime;
+        }
+        else
+        {
+            isMoving = false;
+        }
+    }
+}
