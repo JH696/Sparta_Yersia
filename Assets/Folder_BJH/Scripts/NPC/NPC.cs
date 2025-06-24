6ba16fcafe90def872e5983a2e722f32903e3bb3
백진환 <143722065+JH696@users.noreply.github.com>
2025-06-25 01:14:50 +0900

    feat: NPCData.cs (SO)
    NPC 생성을 위한 SO 클래스 추가
    #1

---
+++ Assets/Folder_BJH/Scripts/NPC/NPC.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using UnityEngine;
+using System.Collections.Generic;
+
+[Serializable]
+[CreateAssetMenu(fileName = "Quest_NameData", menuName = "Data/QuestData")]
+public class NPCData : ScriptableObject
+{
+    [Header("NPC 이름")]
+    public string NpcName;
+
+    [Header("NPC 스프라이트")]
+    public Sprite IconSprite;
+    public Sprite WorldSprite;
+    public Sprite DialogSprite;
+
+    [Header("NPC 스킬")]
+    //public List<Skill> Skills;
+
+    [Header("NPC 기본 스탯")]
+    public float BaseHp;
+    public float BaseMp;
+    public float BaseAtk;
+    public float BaseDef;
+    public float BaseLuck;
+    public float BaseSpeed;
+}
