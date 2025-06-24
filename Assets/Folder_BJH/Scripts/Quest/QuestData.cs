33e97d78bf68960f76cd5a73bd9276c1cd0015f5
백진환 <143722065+JH696@users.noreply.github.com>
2025-06-25 01:06:23 +0900

    feat: QuestData.cs (SO)
    퀘스트 생성을 위한 SO 클래스 추가
    #1

---
+++ Assets/Folder_BJH/Scripts/Quest/QuestData.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+[Serializable]
+[CreateAssetMenu(fileName = "Quest_NameData", menuName = "Data/QuestData")]
+public class QuestData : ScriptableObject
+{
+    public int QuestID;
+    public string QuestName;
+    public string Description;
+    public List<ItemData> Rewards;
+}
