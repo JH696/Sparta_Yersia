5aefcf524ea0666d2b62751c127270155a8ad773
AgathaYi <tjsfid1030@naver.com>
2025-06-24 18:39:47 +0900

    feat: ItemData (SO)
    - 아이템들의 정보를 만들기 위함
    - 메뉴 : Data > ItemData  (스크립터블 오브젝트로 생성)
    - #1

---
+++ Assets/Folder_LSR/cs/ItemData.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "Item_Name", menuName = "Data/ItemData")]
+public class ItemData : ScriptableObject
+{
+    public string ID;
+    public string Name;
+    public Sprite Icon;
+    [TextArea] public string Description;
+
+    // 상점 구현시 주석 해제
+    // public int PriceBuy;
+    // public int PriceSell;
+
+    // 아이템 사용 로직
+    public void OnUse()
+    {
+        Debug.Log($"아이템 사용: {Name}");
+        // TODO: 아이템 사용 로직 구현
+    }
+}
