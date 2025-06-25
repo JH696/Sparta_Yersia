3b3acf597bfef74375a26822039dad224a1d946e
Sangyeop-Lim <dladla5675@gmail.com>
2025-06-25 11:28:27 +0900

    feat : 베이스 캐릭터 스크립트 추가
    베이스 캐릭터 스크립트 추가 #1

---
+++ Assets/Folder_LSY/Scritps/BaseCharacter.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class BaseCharacter : MonoBehaviour
+{
+    [SerializeField] protected CharacterStats Stat = new CharacterStats();
+
+    public float MaxHp => Stat.MaxHp;
+    public float CurrentHp => Stat.CurrentHp;
+    public float MaxMp => Stat.MaxMp;
+    public float CurrentMp => Stat.CurrentMp;
+    public float Attack => Stat.Attack;
+    public float Defense => Stat.Defense;
+    public float Luck => Stat.Luck;
+    public float Speed => Stat.Speed;
+
+    public virtual void TakeDamage(float amount)
+    {
+        float finalDamage = Mathf.Max(1f, amount - Defense);
+        Stat.SetCurrentHp(Stat.CurrentHp - finalDamage);
+    }
+
+    public virtual void Heal(float amount)
+    {
+        Stat.SetCurrentHp(Stat.CurrentHp + amount);
+    }
+}
