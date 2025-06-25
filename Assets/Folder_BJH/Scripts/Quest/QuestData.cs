801206005fe1d5d45dcdde65d1a42f9ca12474f6
백진환 <143722065+JH696@users.noreply.github.com>
2025-06-25 12:11:00 +0900

    refactor: QuestData.cs (SO)
    기능 재정립
    #7

--- Assets/Folder_BJH/Scripts/Quest/QuestData.cs
+++ Assets/Folder_BJH/Scripts/Quest/QuestData.cs
@@ -3,34 +3,46 @@ using UnityEngine;
 using System.Collections.Generic;
 
 [Serializable]
-[CreateAssetMenu(fileName = "QID_Data", menuName = "Data/QuestData")]
-public class QuestData : ScriptableObject
+public class QuestCondition
 {
-    [Header("퀘스트 ID / 이름")]
-    public string QuestID;            // 퀘스트 ID
-    public string QuestName;          // 퀘스트 이름
-
-    [Header("퀘스트 설명")]
-    public string Description;         // 퀘스트 설명
-
-    [Header("퀘스트 보상")]
-    public int RewardExp;              // 퀘스트 완료 시 보상 경험치
-    public int RewardYP;             // 퀘스트 완료 시 보상 골드
-
-    [Header("퀘스트 목표")]
-    public List<QuestBase> QuestBase;       // 퀘스트 기본 정보
+    public String TargetID;
+    public int TargetCount; 
 }
 
 [Serializable]
-public class QuestBase
+[CreateAssetMenu(fileName = "Q0000Data", menuName = "Data/QuestData")]
+public class QuestData : ScriptableObject
 {
-    public EQuestType QuestType;
-    public String TargetID;
+    [Header("퀘스트 제출 위치")]
+    public string TargetID;
+
+    [Header("퀘스트 완료 조건 (아이템 ID / 몬스터 ID, 수량 )")]
+    public List<QuestCondition> QuestConditions;
+
+    [Header("퀘스트 ID / 이름")]
+    public string QuestID;          
+    public string QuestName;        
+
+    [Header("퀘스트 설명")]
+    public string Description;      
+
+    [Header("퀘스트 보상")]
+    public int RewardExp;       
+    public int RewardYP;
+    public List<ItemData> RewardItems;
+
+
+    public void Complete()
+    {
+        //GameManager.Instance.Player.AddExp(RewardExp);
+        //GameManager.Instance.Player.AddYP(RewardYP);
+
+        //foreach (var itemData in RewardItems)
+        //{
+        //    GameManager.Instance.Player.Inventory.AddItem(itemData);
+        //}
+
+        Debug.Log($"퀘스트:{QuestName}, 클리어");
+    }
 }
 
-public enum EQuestType
-{
-    Investigation,
-    Collection,
-    Elimination
-}
