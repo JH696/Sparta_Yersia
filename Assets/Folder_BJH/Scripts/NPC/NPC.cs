7db9578af49c02887aa6aa2ab027ba47935df697
백진환 <143722065+JH696@users.noreply.github.com>
2025-06-25 12:53:54 +0900

    feat: NPC.cs
    NPC Data를 보관하는 NPC클래스 추가
    #1

---
+++ Assets/Folder_BJH/Scripts/NPC/NPC.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class NPC : MonoBehaviour
+{
+    public NPCData NpcData;
+
+    public void StartChat()
+    {
+        Debug.Log($"NPC 대화 시작: {NpcData.NpcName}");
+
+        DialogueManager.Instance.dialogueUI.ToggleDialogueBox();
+        DialogueManager.Instance.LoadDialogueData(NpcData.NpcID);
+        DialogueManager.Instance.PassLine();   
+    }
+}
