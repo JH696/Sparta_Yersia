dd0371673326082af037180e5488836251eed745
백진환 <143722065+JH696@users.noreply.github.com>
2025-06-26 17:40:43 +0900

    feat: 대화창 선택지 버튼
    대화창 나가기 버튼, 퀘스트 수락 버튼 추가
    #21

---
+++ Assets/Folder_BJH/Scripts/UI/ChoiceButtonCreator.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class ChoiceButtonCreator : MonoBehaviour
+{
+    [Header("초이스 버튼 프리팹")]
+    [SerializeField] private GameObject ChoiceButton;
+
+    [Header("생성된 버튼 리스트")]
+    [SerializeField] private List<GameObject> ChoiceButtonsList;
+
+    public void CreateChoiceButton(QuestData questData)
+    {
+        if (questData == null) return;
+
+        GameObject choiceBtn = Instantiate(ChoiceButton, this.transform);
+        choiceBtn.GetComponent<ChoiceButton>().SetChoiceButton(questData);
+
+        ChoiceButtonsList.Add(choiceBtn);
+    }
+
+    public void RemoveChoiceButton()
+    {
+        if (ChoiceButtonsList.Count <= 0) return;
+
+        foreach (GameObject choiceBtn in ChoiceButtonsList)
+        {
+            Destroy(choiceBtn);
+        }
+
+        ChoiceButtonsList.Clear();
+    }
+}
