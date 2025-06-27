d69e05d6920dda63cde79790dd5e301a9e92d856
Sangyeop-Lim <dladla5675@gmail.com>
2025-06-27 16:51:01 +0900

    feat: 펫 데이터 구조 PetData SO 정의 및 진화 정보 설계
    - PetData ScriptableObject 클래스 추가
    - 펫 ID, 이름, StatData 등 기본 속성 정의
    - 3단계 진화를 위한 스프라이트 배열 및 레벨 정보 구성
    - StatMultiplier를 통한 능력치 강화 비율 설정
    - PetSprite, EvoLevel 클래스 추가로 구조화된 진화 시스템 구현 준비
    #17

---
+++ Assets/Folder_LSY/Scripts/Data/PetData.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System;
+
+[CreateAssetMenu(fileName = "PetData", menuName = "Data/PetData")]
+public class PetData : ScriptableObject
+{
+    [Header("펫 ID 및 이름")]
+    public string PetID;
+    public string PetName;
+
+    [Header("펫 스탯")]
+    public CharacterStatData StatData;
+
+    [Header("진화 관련")]
+    [Tooltip("진화 단계별 스프라이트 (ex. 0 = 기본, 1 = 1차 진화, 2 = 2차 진화)")]
+    public PetSprite[] sprites = new PetSprite[3];
+
+    [Tooltip("각 진화 단계에 도달하기 위한 레벨")]
+    public EvoLevel[] evoLevels = new EvoLevel[2];
+
+    [Header("진화 시 적용할 스탯 배율")]
+    public float StatMultiplier = 1.5f;
+}
+
+[Serializable]
+public class PetSprite
+{
+    public Sprite WorldSprite;
+    public Sprite Icon;
+}
+
+[Serializable]
+public class EvoLevel
+{
+    public int Level;
+}
