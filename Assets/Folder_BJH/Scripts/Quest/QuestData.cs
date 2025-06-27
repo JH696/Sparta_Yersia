4498e7f0a5cbb6d9733de8e28109193acb1e06d9
백진환 <143722065+JH696@users.noreply.github.com>
2025-06-27 17:15:06 +0900

    feat: QuestData Editer
    퀘스트 데이터 가독성 향상
    #19

--- Assets/Folder_BJH/Scripts/Quest/QuestData.cs
+++ Assets/Folder_BJH/Scripts/Quest/QuestData.cs
@@ -1,28 +1,52 @@
 ﻿using System;
 using UnityEngine;
 using System.Collections.Generic;
+using UnityEditor;
 
-public enum EQuestConditionType
+public enum EConditionType
 {
     Investigation,      //조사
     Collection,         //수집
     Elimination         //처치
 }
 
-[Serializable]
-public class QuestCondition
+public enum EQuestType
 {
-    public String TargetID;
-    public int TargetCount; 
+    Story,          //스토리 퀘스트
+    Partner,        //파트너 퀘스트
+}
+
+[Serializable]
+public class CollectionCondition
+{
+    public string ItemID;  // 아이템 ID
+    public int ItemCount; // 필요한 아이템 숫자
+}
+
+[Serializable]
+public class EliminationCondition
+{
+    public string EnemyID; // 적 ID
+    public int EnemyCount; // 처치해야 할 적 숫자
 }
 
 [Serializable]
 [CreateAssetMenu(fileName = "Quest_NameData", menuName = "Data/QuestData")]
 public class QuestData : ScriptableObject
 {
-    public EQuestConditionType ConditionType;
+    [Header("퀘스트 완료 조건")]
+    public EConditionType ConditionType;
+    public List<CollectionCondition> ItemTarget = new List<CollectionCondition>();
+    public List<EliminationCondition> MonsterTarget = new List<EliminationCondition>();
 
+    [Header("퀘스트 유형")]
+    public EQuestType QuestType;
+
+    [Header("퀘스트 지급자 NPC ID")]
     public String AssignerID;
+
+    [Header("퀘스트 보고자 NPC ID")]
+    public string ReceiverID;
 
     [Header("퀘스트 ID / 이름")]
     public string QuestID;          
@@ -31,13 +55,48 @@ public class QuestData : ScriptableObject
     [Header("퀘스트 설명")]
     public string Description;
 
-    [Header("퀘스트 조건 (조사 퀘스트는 설정 X)")]
-    public List<QuestCondition> Conditions = new List<QuestCondition>();
-
     [Header("퀘스트 보상")]
     public int RewardExp;
     public int RewardYP;
     public List<ItemData> RewardItems;
+}
+
+[CustomEditor(typeof(QuestData))]
+public class QuestDataEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        serializedObject.Update();
+
+        EditorGUILayout.PropertyField(serializedObject.FindProperty("QuestType"));
+        EditorGUILayout.PropertyField(serializedObject.FindProperty("AssignerID"));
+        EditorGUILayout.PropertyField(serializedObject.FindProperty("ReceiverID"));
+        EditorGUILayout.PropertyField(serializedObject.FindProperty("QuestID"));
+        EditorGUILayout.PropertyField(serializedObject.FindProperty("QuestName"));
+        EditorGUILayout.PropertyField(serializedObject.FindProperty("Description"));
+        EditorGUILayout.PropertyField(serializedObject.FindProperty("RewardExp"));
+        EditorGUILayout.PropertyField(serializedObject.FindProperty("RewardYP"));
+        EditorGUILayout.PropertyField(serializedObject.FindProperty("RewardItems"));
+
+
+        SerializedProperty questTypeProp = serializedObject.FindProperty("ConditionType");
+        EditorGUILayout.PropertyField(questTypeProp);
+
+        EConditionType questType = (EConditionType)questTypeProp.enumValueIndex;
+
+        // 조건에 따라 필드 노출
+        switch (questType)
+        {
+            case EConditionType.Collection:
+                EditorGUILayout.PropertyField(serializedObject.FindProperty("ItemTarget"));
+                break;
+            case EConditionType.Elimination:
+                EditorGUILayout.PropertyField(serializedObject.FindProperty("MonsterTarget"));
+                break;
+        }
+
+        serializedObject.ApplyModifiedProperties();
+    }
 }
 
 
