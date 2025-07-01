dd548cd1bac9d8997c84c95dc4a47e09e80f4f0c
Sangyeop-Lim <dladla5675@gmail.com>
2025-07-01 12:18:52 +0900

    feat: 캐릭터 스탯 공통 인터페이스 ICharacterStatData 정의
    - MaxHp, MaxMana, Attack, Defense, Luck, Speed 속성을 포함한 ICharacterStatData 인터페이스 추가
    - CharacterStatData, PlayerData 등에서 공통 스탯 접근을 위한 추상화 목적
    - 다양한 캐릭터 유형 간 스탯 처리 일관성 확보를 위한 기반 구성
    #16

---
+++ Assets/Folder_LSY/Scripts/ICharacterStatData.cs
@@ -0,0 +1,9 @@
+﻿public interface ICharacterStatData
+{
+    float MaxHp { get; }
+    float MaxMana { get; }
+    float Attack { get; }
+    float Defense { get; }
+    float Luck { get; }
+    float Speed { get; }
+}
