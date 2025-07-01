08c1811e6a82f43898d873cca1335d7e804b3762
백진환 <143722065+JH696@users.noreply.github.com>
2025-07-01 10:11:15 +0900

    rename: PlayerQuest, QuestManager.cs
    변수, 메서드명 수정
    #19

--- Assets/Folder_BJH/Scripts/Quest/QuestData.cs
+++ Assets/Folder_BJH/Scripts/Quest/QuestData.cs
@@ -17,14 +17,14 @@ public enum EQuestType
 }
 
 [Serializable]
-public class CollectionCondition
+public class Collection
 {
     public string ItemID;  // 아이템 ID
     public int ItemCount; // 필요한 아이템 숫자
 }
 
 [Serializable]
-public class EliminationCondition
+public class Elimination
 {
     public string EnemyID; // 적 ID
     public int EnemyCount; // 처치해야 할 적 숫자
@@ -36,8 +36,8 @@ public class QuestData : ScriptableObject
 {
     [Header("퀘스트 완료 조건")]
     public EConditionType ConditionType;
-    public List<CollectionCondition> TargetItem = new List<CollectionCondition>();
-    public List<EliminationCondition> TargetEnemy = new List<EliminationCondition>();
+    public List<Collection> TargetItem = new List<Collection>();
+    public List<Elimination> TargetEnemy = new List<Elimination>();
 
     [Header("퀘스트 유형")]
     public EQuestType QuestType;
