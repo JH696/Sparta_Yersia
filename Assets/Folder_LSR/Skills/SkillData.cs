9866fd7ca7b1a354ea29d9f2271cdf0b76855442
AgathaYi <tjsfid1030@naver.com>
2025-07-01 22:02:53 +0900

    feat: SkillData SO
    스킬 데이터 스크립터블오브젝트
    #27

--- Assets/Folder_LSR/Skills/SkillData.cs
+++ Assets/Folder_LSR/Skills/SkillData.cs
@@ -1,18 +1,42 @@
-using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
-
-public class SkillData : MonoBehaviour
+public enum ESkillType
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    Fire,
+    Ice,
+    Nature,
+    Physical,
+    All
+}
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+public enum ESkillTier
+{
+    Beginner = 1, // 1=초급
+    Intermediate = 2, // 2=중급
+    Advanced = 3 // 3=상급
+}
+
+[CreateAssetMenu(menuName = "Data/SkillData")]
+public class SkillData : ScriptableObject
+{
+    public string SkillName;
+    public ESkillType SkillType;
+    public ESkillTier Tier;
+    public int UnlockCost;    // 해금에 필요한 포인트
+    public int MaxLevel;
+    public Sprite Icon;
+    [TextArea] public string Description;
+}
+
+[CreateAssetMenu(menuName = "Data/SkillLibrary")]
+public class SkillLibrary : ScriptableObject
+{
+    public SkillData[] AllSkills;
+
+    public IEnumerable<SkillData> GetByCategory(ESkillType type)
+      => AllSkills.Where(skills => skills.SkillType == type);
+
+    public IEnumerable<SkillData> GetByTier(ESkillTier tier)
+      => AllSkills.Where(s => s.Tier == tier);
 }
