3461867c1b67d9b0868f91a270f9061d787ba574
Sangyeop-Lim <dladla5675@gmail.com>
2025-07-07 19:41:35 +0900

    feat: BattleTrigger 구현 및 EScene에 테스트용 배틀 씬 추가
    - 플레이어가 트리거에 진입하면 지정된 배틀 씬으로 전환되는 BattleTrigger 스크립트 구현
    - EScene 열거형에 Scene_LSY_Battle 항목 추가
    - SceneLoader를 통해 씬 전환 처리
    #28

---
+++ Assets/Folder_LSY/Scripts/BattleTrigger.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class BattleTrigger : MonoBehaviour
+{
+    [SerializeField] private EScene battleScene;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (!other.CompareTag("Player")) return;
+        SceneLoader.LoadScene(battleScene);
+    }
+}
---
+++ Assets/Folder_LSY/Scripts/Data/EScene.cs
@@ -0,0 +1,8 @@
+﻿public enum EScene
+{
+    //MainScene,
+    //BattleScene
+
+    Scene_LSY,
+    Scene_LSY_Battle
+}
