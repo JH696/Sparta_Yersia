87438319a79a0a9ae8a6e2426d5da36981b14e02
Sangyeop-Lim <dladla5675@gmail.com>
2025-07-07 15:48:19 +0900

    feat: Portal 오브젝트에 IInteractable 구현 및 이동 기능 추가
    - Portal.cs에서 IInteractable 인터페이스 구현
    - Interact() 호출 시 플레이어를 destination 위치로 이동
    - destination은 에디터에서 설정 가능한 Transform 필드로 구성
    - 포탈도 상호작용 시스템에 통합되어 일관된 처리 가능
    #28

---
+++ Assets/Folder_LSY/Scripts/Portal.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class Portal : MonoBehaviour, IInteractable
+{
+    [SerializeField] private Transform destination;
+
+    public void Interact()
+    {
+        GameManager.Instance.Player.transform.position = destination.position;
+    }
+}
