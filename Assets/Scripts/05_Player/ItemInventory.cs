eb8d8f627d7c9f8725f248cfaae63bddb4866be5
백진환 <143722065+JH696@users.noreply.github.com>
2025-07-17 15:57:27 +0900

    feat: CharacterEquipment
    장착 중인 장비 저장 클래스 추가
    #29

--- Assets/Scripts/05_Player/ItemInventory.cs
+++ Assets/Scripts/05_Player/ItemInventory.cs
@@ -51,10 +51,115 @@ public class ItemStatus
     }
 }
 
+public class CharacterEquipment
+{
+    public EquipItemData Weapon;
+    public EquipItemData Hat;
+    public EquipItemData Accessory;
+    public EquipItemData Clothes;
+    public EquipItemData Shoes;
+
+    public event Action EquipmentChanged; // 장비 교체 이벤트
+
+    public CharacterEquipment()
+    {
+        Weapon = null;
+        Hat = null;
+        Accessory = null;
+        Clothes = null;
+        Shoes = null;
+    }
+
+
+    /// <summary>
+    /// 장비 아이템을 장착하고 기존 장착한 아이템을 반환합니다.
+    /// </summary>
+    public EquipItemData Equip(EquipItemData item)
+    {
+        // 기존 장비 해제
+        EquipItemData previous = null;
+
+        switch (item.Type)
+        {
+            case E_EquipType.Weapon:
+                previous = Weapon;
+                Weapon = item;
+                break;
+            case E_EquipType.Hat:
+                previous = Hat;
+                Hat = item;
+                break;
+            case E_EquipType.Accessory:
+                previous = Accessory;
+                Accessory = item;
+                break;
+            case E_EquipType.Clothes:
+                previous = Clothes;
+                Clothes = item;
+                break;
+            case E_EquipType.Shoes:
+                previous = Shoes;
+                Shoes = item;
+                break;
+            default:
+                Debug.LogWarning("[CharacterEquipment] 존재하지 않는 유형의 장비입니다: " + item.Type);
+                return null;
+        }
+
+        if (previous != null)
+        {
+            previous.IsEquipped = false;
+        }
+
+        item.IsEquipped = true;
+        EquipmentChanged?.Invoke();
+        return previous;
+    }
+
+    /// <summary>
+    /// 원하는 타입에 장비를 장착 해제하고 반환합니다.
+    /// </summary>
+    public EquipItemData Unequip(E_EquipType type)
+    {
+        EquipItemData target;
+
+        switch (type)
+        {
+            case E_EquipType.Weapon:
+                target = Weapon;
+                break;
+            case E_EquipType.Hat:
+                target = Hat;
+                break;
+            case E_EquipType.Accessory:
+                target = Accessory;
+                break;
+            case E_EquipType.Clothes:
+                target = Clothes;
+                break;
+            case E_EquipType.Shoes:
+                target = Shoes;
+                break;
+            default:
+                Debug.LogWarning("[CharacterEquipment] 존재하지 않는 유형의 장비입니다: " + type);
+                return null;
+        }
+
+        target.IsEquipped = false;
+        target = null;
+
+        return target;
+    }
+}
+
 public class ItemInventory
 {
     [SerializeField] private List<ItemStatus> items = new List<ItemStatus>();
+
+    [SerializeField] private List<EquipItemData> equipedItems = new List<EquipItemData>();
+
     public List<ItemStatus> Items => items;
+    public List<EquipItemData> EquipedItems => equipedItems;
 
     public event Action InventoryChanged;
 
