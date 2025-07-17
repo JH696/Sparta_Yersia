93d3eaa5258fff1aa7d4be3852c1fcf9be82d5d7
Sangyeop-Lim <dladla5675@gmail.com>
2025-07-17 15:58:37 +0900

    feat: GlobalSaveManager 클래스 추가 및 플레이어 저장/로드 기능 구현
    - GlobalSaveManager 클래스 신규 추가: 전역에서 접근 가능한 플레이어 저장 시스템
    - Save(Player) 메서드: makeSaveData() 호출하여 현재 상태 저장
    - Load(Player) 메서드: 저장된 PlayerSaveData 기반으로 상태 복원
    - 저장 정보가 null일 경우 에러 로그 출력 후 처리 중단
    - playerSaveData는 static으로 선언하여 씬 간 데이터 유지 가능
    #28

---
+++ Assets/Scripts/00_Manager/GlobalSaveManager.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class GlobalSaveManager : MonoBehaviour
+{
+    public static PlayerSaveData playerSaveData;
+    public static void Save(Player player)
+    {
+        playerSaveData = player.makeSaveData();
+    }
+
+    public static void Load(Player player)
+    {
+        if (playerSaveData == null)
+        {
+            Debug.LogError("PlayerSaveData is null. Cannot load data.");
+            return;
+        }
+        player.LoadData(playerSaveData);
+    }
+}
