ae084639501a6411f8ea9d92adb52626f1740e53
Sangyeop-Lim <dladla5675@gmail.com>
2025-07-17 16:06:32 +0900

    feat: 캐릭터 스탯 정보 관리를 위한 StatData 클래스 추가
    - StatData 클래스를 ScriptableObject 기반으로 정의하여 스탯 데이터를 외부에서 관리 가능하도록 설계
    - maxHp, maxMana, attack, defense, luck, speed 등 기본 스탯 필드 추가
    - Multiplier 필드 도입으로 레벨업 시 사용되는 성장 배율 설정 가능
    - 캐릭터 클래스와 독립적인 데이터 관리 구조 기반 마련됨
    #16

---
+++ Assets/Scripts/02_Data/StatData.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using UnityEngine;
+
+[Serializable]
+public abstract class StatData : ScriptableObject
+{
+
+    [Header("기초 스탯")]
+    [SerializeField] public float maxHp = 100f;
+    [SerializeField] public float maxMana = 80f;
+    [SerializeField] public float attack = 20f;
+    [SerializeField] public float defense = 10f;
+    [SerializeField] public float luck = 5f;
+    [SerializeField] public float speed = 10f;
+
+    [Header("스탯 상승치")]
+    public float Multiplier = 1.1f; // 레벨업 시 스탯 상승치
+}
