8147bda5f722aa2f53825aa8e9ec3813c14b667a
Sangyeop-Lim <dladla5675@gmail.com>
2025-07-17 16:18:55 +0900

    feat: PlayerSaveData에 퀘스트 진행 및 펫 정보 저장 구조 추가
    - QuestStatusData: 퀘스트 ID와 완료 여부 저장 구조 정의
    - EliQuestProgressData: 엘리 퀘스트 진행 상황을 위한 적 처치 수 데이터 구조화
    - 내부적으로 EliCountData 리스트 구성 (EnemyID, KillCount)
    - PlayerSaveData 클래스 정의
    - 경험치, 레벨, HP/MP, 화폐(YP) 등의 기본 상태 저장
    - Inventory: 아이템 수량을 딕셔너리 형태로 저장
    - 퀘스트 진행 정보 (questStatusDatas, eliQuestProgressDatas) 리스트화
    - 펫 보유 및 장착 상태를 ID 리스트로 관리 (ownedPetIDs, equipPetIDs)
    #16

---
+++ Assets/Scripts/05_Player/PlayerSaveData.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+
+[System.Serializable]
+public class QuestStatusData
+{
+    public string QuestID; // 퀘스트 ID
+    public bool IsCompleted; // 퀘스트 완료 여부
+}
+
+[System.Serializable]
+public class EliQuestProgressData
+{
+    public string QuestID; // 퀘스트 ID
+    public List<EliCountData> eliCountDatas = new List<EliCountData>(); // 엘리 카운트 데이터 리스트
+}
+
+[System.Serializable]
+public class EliCountData
+{
+    public string EnemyID;
+    public int KillCount;
+}
+
+[System.Serializable]
+public class PlayerSaveData
+{
+    public int Level;
+    public int CurrentExp;
+    public float CurrnetHP;
+    public float CurrentMP;
+    public int YP; // 플레이어의 화폐
+    public Dictionary<string, int> Inventory = new Dictionary<string, int>(); // 아이템 ID와 개수의 딕셔너리
+    public List<QuestStatusData> questStatusDatas = new List<QuestStatusData>(); // 퀘스트 상태 데이터 리스트
+    public List<EliQuestProgressData> eliQuestProgressDatas = new List<EliQuestProgressData>(); // 엘리 퀘스트 진행 데이터 리스트
+    public List<string> ownedPetIDs = new List<string>(); // 소유한 펫 ID 리스트
+    public List<string> equipPetIDs = new List<string>(); // 장착한 펫 ID 리스트
+}
