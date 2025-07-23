f896cbd0b345356c7bd1d2e2ae1a760efd8144bb
YBdhhh <97926god@gmail.com>
2025-07-23 20:29:35 +0900

    feat: 인트로매니저 추가
    feat: 인트로매니저 추가 이미지 이동 작업중

---
+++ Assets/Scripts/00_Manager/IntroManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IntroManager : MonoBehaviour
+{
+    FadeIn fadein;
+    CameraFollow cameraFollow;
+    [SerializeField] private CanvasGroup fade;  //로고+버튼들
+    [SerializeField] private CanvasGroup fadeBlack;  //까만화면
+    // Start is called before the first frame update
+    private void Awake()
+    {
+        cameraFollow = GetComponent<CameraFollow>();
+        fadein = GetComponent<FadeIn>();
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        cameraFollow.Scroll();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
